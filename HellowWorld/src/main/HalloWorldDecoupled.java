cfbb2477201ad9589d6fccb4b83800da69dcfad0
Dr.J (정의원) <82809765+jing-si@users.noreply.github.com>
2022-11-30 22:13:03 +0900

    저렇게

--- HellowWorld/src/main/HalloWorldDecoupled.java
+++ HellowWorld/src/main/HalloWorldDecoupled.java
@@ -5,7 +5,7 @@ public class HalloWorldDecoupled {
 	public static void main(String[] args) {
 		MessageProvider mp = new HelloWorldMessageProviderImpl();
 		MessageRenderer mr = new MessageRendererImpl();
-		mp.setMessage("이렇게");
+		mp.setMessage("저렇게 저렇게");
 		mr.setMessageProvider(mp);
 		mr.render();
 	}
