dd4998b58df8a35e4ba79c9110905df6c618c1ed
Dr.J (정의원) <82809765+jing-si@users.noreply.github.com>
2022-12-06 23:47:01 +0900

    Q2-12

--- HellowWorld/src/main/HalloWorldDecoupled.java
+++ HellowWorld/src/main/HalloWorldDecoupled.java
@@ -3,20 +3,13 @@ package main;
 public class HalloWorldDecoupled {
 
 	public static void main(String[] args) {
-		MessageProvider mp = new HelloWorldMessageProviderImpl();
-		MessageRenderer mr = new MessageRendererImpl();
+		MessageProvider mp = MessageSupportFactory.getInstacne().getProvider();
+		MessageRenderer mr = MessageSupportFactory.getInstacne().getRenderer();
 		mp.setMessage("저렇게 저렇게");
 		mr.setMessageProvider(mp);
 		mr.render();
 		
-		Class<?> classA = null;
-		try {
-			classA = Class.forName("main.HelloWorldMessageProviderImpl");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(classA.getName());
+		
 		
 	}
 
--- HellowWorld/src/main/MessageSupportFactory.java
+++ HellowWorld/src/main/MessageSupportFactory.java
@@ -13,7 +13,7 @@ public class MessageSupportFactory {
 		props = new Properties();
 		
 		try {
-			props.load(this.getClass().getResourceAsStream("/msf.properties"));
+			props.load(this.getClass().getResourceAsStream("msf.properties"));
 			String rendererClass = props.getProperty("renderer.class");
 			String providerClass = props.getProperty("provider.class");
 			
