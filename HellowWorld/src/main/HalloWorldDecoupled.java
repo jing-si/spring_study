70eca97f77865ad025472e5c882bab997d6c62d1
Dr.J (정의원) <82809765+jing-si@users.noreply.github.com>
2022-12-06 23:39:30 +0900

    예제1차

--- HellowWorld/src/main/HalloWorldDecoupled.java
+++ HellowWorld/src/main/HalloWorldDecoupled.java
@@ -8,6 +8,16 @@ public class HalloWorldDecoupled {
 		mp.setMessage("저렇게 저렇게");
 		mr.setMessageProvider(mp);
 		mr.render();
+		
+		Class<?> classA = null;
+		try {
+			classA = Class.forName("main.HelloWorldMessageProviderImpl");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(classA.getName());
+		
 	}
 
 }
